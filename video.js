a4ea864b5b313e4b9bf1bca8c3e4ce6227ae4aa0
Jasper Holton <jasper@holton.io>
2019-06-28 18:48:24 -0700

    Create video.js

---
+++ video.js
@@ -0,0 +1,26 @@
+var config = {
+    type: Phaser.CANVAS,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+  this.load.image('test', 'assets/test.png');
+}
+
+function create ()
+{
+this.add.image(400, 300, 'test');
+}
+
+function update ()
+{
+}
