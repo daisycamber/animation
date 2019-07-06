b93889684abd31c7f777fe4d9600021b48b8af8f
Jasper Holton <jasper@holton.io>
2019-07-06 12:50:55 -0700

    Update video.js

--- video.js
+++ video.js
@@ -22,7 +22,7 @@ var circles = [];
 function create ()
 {
     for(var i = 0; i < 200; i++){
-        circles[i] = this.add.circle(Phaser.Math.Between(0, 3000), 1500, Phaser.Math.Between(1,50), '#'+(Math.random()*0xFFFFFF<<0).toString(16));
+        circles[i] = this.add.circle(Phaser.Math.Between(0, 3000), 1500, Phaser.Math.Between(1,50),(Math.random()*0xffffff<<0).toString(16));
         circles[i].xv = Phaser.Math.Between(-maxParticleSpeed,maxParticleSpeed);
         circles[i].yv = Phaser.Math.Between(-maxParticleSpeed,maxParticleSpeed);
     }
