03ad3984a8d5085c0864a539ad5115895d5e0aad
Jasper Holton <jasper@holton.io>
2019-07-06 15:02:11 -0700

    Update video.js

--- video.js
+++ video.js
@@ -68,7 +68,7 @@ function update ()
             }
         }
     }
-    if(frame < 60 * 60 * 3){
+    if(frame < 60 * 60 * 3.2){
         
         if(downloadOn){
             var image    = this.game.canvas.toDataURL();
