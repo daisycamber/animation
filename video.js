88dd7d191b0a25bce221868c6c0f91963f212b87
Jasper Holton <jasper@holton.io>
2019-07-06 13:14:05 -0700

    Update video.js

--- video.js
+++ video.js
@@ -24,6 +24,7 @@ function preload ()
 var circles = [];
 function create ()
 {
+    console.log(Phaser.Math.Between(0xCCCCCC,0xFFFFFF).toString(16));
     for(var i = 0; i < 100; i++){
         circles[i] = this.add.circle(Phaser.Math.Between(0, 3000), 1500, Phaser.Math.Between(minParticleSize,maxParticleSize),Phaser.Math.Between(0xCCCCCC,0xFFFFFF).toString(16));
         circles[i].xv = Phaser.Math.Between(-maxParticleSpeed,maxParticleSpeed);
