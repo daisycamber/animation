66e10f2422cfd733892f91fc0132125261e8124b
Jasper Holton <jasper@holton.io>
2019-07-06 15:40:31 -0700

    Update video.js

--- video.js
+++ video.js
@@ -47,7 +47,7 @@ function update ()
     if(frame > lastBeat + fpb) {
         // bring last ring to front
         rings[currentRing].radius=50;
-        rings[currentRing].depth +=200;
+        rings[currentRing].depth +=400;
         currentRing++;
         if(currentRing > 199) currentRing = 0;
         for(var i = 0; i < circles.length; i++){
