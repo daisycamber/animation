8e428f9c547ceb11af8e5a5dcc630a1a2c66ef4c
Jasper Holton <jasper.camber.holton@gmail.com>
2019-07-08 14:37:42 -0700

    Update video.js

--- video.js
+++ video.js
@@ -1,3 +1,4 @@
+var videoLength = 3.2;
 var minParticleSize = 1;
 var maxParticleSpeed = 10;
 var width = 1920;
@@ -18,23 +19,19 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.game.canvas.id = 'canvas';
-    this.load.image('test', 'test.png');
-    
+    this.game.canvas.id = 'canvas';    
 }
 var currentRing = 0;
 var rings = [];// .depth = NUMBER
 var circles = [];
 function create ()
 {
-    for(var i = 0; i < 20; i++){
-        rings[i] = this.add.circle(width/2, height/2, 50 + 22 * (99 - i),"0x"+Phaser.Math.Between(0x999999,0xFFFFFF).toString(16));
-    }
-    for(var i = 0; i < 200; i++){
-        circles[i] = this.add.circle(Phaser.Math.Between(0, width), height/2, Phaser.Math.Between(minParticleSize,maxParticleSize),"0x"+Phaser.Math.Between(0xCCCCCC,0xFFFFFF).toString(16));
-        circles[i].xv = Phaser.Math.Between(-maxParticleSpeed,maxParticleSpeed);
-        circles[i].yv = Phaser.Math.Between(-maxParticleSpeed,maxParticleSpeed);
-    }
+    graphics.lineStyle(5, 0xFF00FF, 1.0);
+    graphics.beginPath();
+    graphics.moveTo(100, 100);
+    graphics.lineTo(200, 200);
+    graphics.closePath();
+    graphics.strokePath();
     
     audio = new Audio();
     context = new (window.AudioContext || window.webkitAudioContext)();
@@ -60,50 +57,28 @@ var fpb = (60*60)/bpm; // frames per beat
 var halfBeat = fpb/2;
 var lastBeat = 0;
 var lastHalfBeat = 0;
+
+// Called every frame except the beat
+function move(){
+    
+}
+// Called every beat
+function beat(){
+    
+}
 function update ()
 {
     if(frame > lastBeat + fpb) {
-        for(var i = 0; i < circles.length; i++){
-            circles[i].depth = circles.length - i;
-            circles[i].y=height/2;
-            circles[i].xv = Phaser.Math.Between(-maxParticleSpeed,maxParticleSpeed);
-            circles[i].yv = Phaser.Math.Between(-maxParticleSpeed,maxParticleSpeed);
-            
-        }
-        for(var i = 0; i < rings.length; i++){
-            rings[i].radius=50 + (22 * i);
-            rings[i].depth = rings.length - i;
-            rings[i].setFillStyle("0x"+Phaser.Math.Between(0x999999,0xFFFFFF).toString(16));
-        }
-        lastBeat = frame;
+        beat();
     }
     else{
-        for(var i = 0; i < rings.length; i++){
-            rings[i].radius = rings[i].radius + 5; // Make ring bigger
-        }
-        for(var i = 0; i < circles.length; i++){
-            circles[i].x+=circles[i].xv;
-            circles[i].y+=circles[i].yv;
-            if(circles[i].y>height+50){
-                circles[i].y=-50;
-            }
-            if(circles[i].y<-50){
-                circles[i].y=height+50;
-            }
-            if(circles[i].x>width+50){
-                circles[i].x=-50;
-            }
-            if(circles[i].x<-50){
-                circles[i].x=width + 50;
-            }
-        }
+        move();
     }
-    if(frame < 60 * 60 * 3.2){
-        
+    if(frame < 60 * 60 * videoLength){  
         if(downloadOn){
             var image    = this.game.canvas.toDataURL();
             download(image, frame + ".png", "image/png");
         }
-        frame++;
     }
+    frame++;
 }
