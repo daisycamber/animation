058bd800743e1992c675f18a326b316c39a08495
Jasper Holton <jasper.camber.holton@gmail.com>
2019-07-08 18:34:14 -0700

    Update video.js

--- video.js
+++ video.js
@@ -26,32 +26,74 @@ var rings = [];// .depth = NUMBER
 var circles = [];
 var graphics;
 
-function drawLine(x,y,xx,xy){
+function drawLine(x,y,xx,xy, angle){
     var path = new Phaser.Curves.Path(x, y);
     path.lineTo(xx, xy);
     path.draw(graphics);
 }
     
+var centerX = 1920/2;
 
-function draw(startX, startY, len, angle) {
-    drawLine(startY,startY,0,-len);
-  if(len < 10) {
+var trunkHeight = 100;
+var branchLengthRatio = 0.75;
+var branchAngleDifference = 0.27;
+var branchingDepth = 10;
+
+function drawTree(x1, y1, x2, y2, branchLength,
+                  branchAngle, depth){
+  if(depth == 0)
     return;
+  else{
+    c.beginPath();
+    drawLine(x1,x2,y1,y2);
+    branchLength *= branchLengthRatio;
+    
+    function branch(angle){
+      var branchX2 = x2 + branchLength * Math.cos(angle);
+      var branchY2 = y2 + branchLength * Math.sin(angle);
+      drawTree(x2, y2, branchX2, branchY2, branchLength,
+               angle, depth - 1);
+    }
+    
+    // Right branch
+    branch(branchAngle + branchAngleDifference);
+    
+    // Left branch
+    branch(branchAngle - branchAngleDifference);
   }
-  
-  draw(0, -len, len*0.8, -15);
-  draw(0, -len, len*0.8, 15);
 }
+
+function redrawTree(){
+  
+  c.clearRect(0,0, canvas.width, canvas.height);
+  
+  var x1 = centerX;
+  var y1 = canvas.height;
+  var x2 = centerX;
+  var y2 = canvas.height - trunkHeight;
+  drawTree(x1, y1, x2, y2, trunkHeight,
+           - Math.PI / 2, branchingDepth);
+}
+
+/*
+canvas.addEventListener("mousemove",function(e){
+  branchLengthRatio = e.x / 300;
+  branchAngleDifference = e.y / canvas.height * Math.PI;
+  redrawTree();
+  console.log("branchLengthRatio = "+branchLengthRatio);
+  console.log("branchAngleDifference = "+branchAngleDifference);
+});*/
+
+
+
 function create ()
 {
     this.cameras.main.setBackgroundColor("0xffffff");
     graphics = this.add.graphics();
     graphics.lineStyle(2, 0x000000, 1);
-    drawLine(0,0,100,100);
-    
-    draw(350,600,120,0);
     
     
+    redrawTree();
     
     audio = new Audio();
     context = new (window.AudioContext || window.webkitAudioContext)();
